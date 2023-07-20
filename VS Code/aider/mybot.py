0c44c747afb042fc9577cc29f93be4adecc8e562
Sandeep Saha <san.saha@gmail.com>
2023-07-20 17:27:39 -0400

    Refactor respond_to_user function and send function to remove unnecessary parameters and update API engine and temperature.n
    # Aider chat conversation:

--- VS Code/aider/mybot.py
+++ VS Code/aider/mybot.py
@@ -1,76 +1,33 @@
 import openai
 import tkinter as tk
-from tkinter import ttk
 
 openai.api_key = 'your-api-key'
 
-def respond_to_user(prompt, role, top_p, temperature, include_markdown, include_questions, model):
-    prompt_addendum = ""
-    if include_markdown:
-        prompt_addendum += " Please include headings, formatting, etc."
-    if include_questions:
-        prompt_addendum += " If you have any clarifying questions about the task, ask now before answering."
-    prompt = f"As a {role}, {prompt}{prompt_addendum}"
+def respond_to_user(prompt):
     response = openai.Completion.create(
-      engine=model,
+      engine="text-davinci-002",
       prompt=prompt,
-      temperature=temperature,
-      top_p=top_p,
+      temperature=0.5,
       max_tokens=100
     )
     return response.choices[0].text.strip()
 
 def send():
-    user_input = base_prompt_entry.get()
-    role = role_combobox.get()
-    top_p = top_p_scale.get()
-    temperature = temperature_scale.get()
-    include_markdown = include_markdown_var.get()
-    include_questions = include_questions_var.get()
-    model = model_combobox.get()
-    response = respond_to_user(user_input, role, top_p, temperature, include_markdown, include_questions, model)
+    user_input = user_input_entry.get()
+    response = respond_to_user(user_input)
     chat_log.insert(tk.END, "User: " + user_input + "\n")
     chat_log.insert(tk.END, "Bot: " + response + "\n")
 
 root = tk.Tk()
 root.title("Chatbot")
 
-left_frame = tk.Frame(root, width=200)
-left_frame.pack(side=tk.LEFT, fill=tk.Y)
+chat_log = tk.Text(root)
+chat_log.pack()
 
-right_frame = tk.Frame(root, width=800)
-right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+user_input_entry = tk.Entry(root)
+user_input_entry.pack()
 
-role_combobox = ttk.Combobox(left_frame, values=["Lawyer", "Software Developer", "Marketing Specialist"])
-role_combobox.pack()
-
-top_p_scale = tk.Scale(left_frame, from_=0, to=1, resolution=0.01, orient=tk.HORIZONTAL)
-top_p_scale.pack()
-
-temperature_scale = tk.Scale(left_frame, from_=0, to=1, resolution=0.01, orient=tk.HORIZONTAL)
-temperature_scale.pack()
-
-include_markdown_var = tk.BooleanVar()
-include_markdown_checkbox = tk.Checkbutton(left_frame, text="Include markdown", variable=include_markdown_var)
-include_markdown_checkbox.pack()
-
-include_questions_var = tk.BooleanVar()
-include_questions_checkbox = tk.Checkbutton(left_frame, text="Include questions", variable=include_questions_var)
-include_questions_checkbox.pack()
-
-base_prompt_entry = tk.Entry(left_frame)
-base_prompt_entry.pack()
-
-generate_button = tk.Button(left_frame, text="Generate", command=send)
-generate_button.pack()
-
-resulting_prompt_text = tk.Text(left_frame)
-resulting_prompt_text.pack()
-
-model_combobox = ttk.Combobox(left_frame, values=["text-davinci-002", "text-curie-002"])
-model_combobox.pack()
-
-chat_log = tk.Text(right_frame)
-chat_log.pack(fill=tk.BOTH, expand=True)
+send_button = tk.Button(root, text="Send", command=send)
+send_button.pack()
 
 root.mainloop()
